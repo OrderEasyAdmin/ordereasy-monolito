e7927997b9877cceae93ebce81cc216c2d007c4e
Filipe Almeida <ordereasy@ordereasy.com.br>
2024-01-25 20:55:27 -0300

    cricao repositorio e add projeto

---
+++ clientedemo/db/base.php
@@ -0,0 +1,3 @@
+<?php
+define('HOME', '');
+setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
---
+++ funcoes/Conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'root';
+$senha 	  = '';
+$banco    = 'ordereasy';
+
+// FUNCOES DO SISTEMA DE CADASTRO ###########
+
+$nomesite  = 'NOMEDASUAEMPRESA';
+$urlmaster = 'https://localhost/ordereasy'; // APENAS A URL PRINCIPAL SEM A BARRA NO FINAL ---- ----
+
+date_default_timezone_set('America/Sao_Paulo');
